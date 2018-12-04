redhat-openstack/infrared

[manila] ceph and nfs-ganesha backend
Change-Id: Ib9cf016b018202156f4a7316e73455926ddedf06

plugins/tripleo-overcloud/plugin.spec
@@ -466,7 +466,18 @@ subparsers:
                       default: rbd
                       help: |
                         The storage backend type used for glance, enabling to set Swift or RadosGW as the backend when deploying internal Ceph. Default value is 'rbd'
-
+                  storage-protocol-backend:
+                      type: Value
+                      default: NA
+                      choices:
+                          - nfs-ganesha
+                          - nfs
+                          - iscsi
+                          - NA
+                      help: |
+                        The storage protocol that we would like to use.
+                        nfs-ganesha works only if the sotrage backand is ceph
+                        NOTE: nfs and iscsi are not implemented but added to be decoupled from storage backend parameter
                   nova-nfs-backend:
                       type: Bool
                       help: |

rhos-infra/tripleo

plugin.spec
@@ -482,7 +482,18 @@ subparsers:
                       default: rbd
                       help: |
                         The storage backend type used for glance, enabling to set Swift or RadosGW as the backend when deploying internal Ceph. Default value is 'rbd'
-
+                  storage-protocol-backend:
+                      type: Value
+                      default: NA
+                      choices:
+                          - nfs-ganesha
+                          - nfs
+                          - iscsi
+                          - NA
+                      help: |
+                        The storage protocol that we would like to use.
+                        nfs-ganesha works only if the storage backand is ceph
+                        NOTE: nfs and iscsi are not implemented but added to be decoupled from storage backend parameter
                   nova-nfs-backend:
                       type: Bool
                       help: |